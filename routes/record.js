6140c4aad208f92e5c10dbeb97ac23005d75b147
bryanmiranda138 <100953022+bryanmiranda138@users.noreply.github.com>
2024-05-25 22:13:09 -0600

    Update record.js

--- routes/record.js
+++ routes/record.js
@@ -15,7 +15,8 @@ recordRoutes.route('/').get(async function (_req, res) {
   + "<br> Hostname = " + os.hostname()
   + "<br> OS = " + os.platform()
   + "<br> version = " + os.version()
-  + "<br> Total Memoria "+ os.totalmem()  
+  + "<br> Total Memoria "+ os.totalmem()
+  + "<h2> CM16127, Bryan Andres Candelario Miranda</h2>"  
   );  
 });
 
